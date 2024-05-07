0c2108965f7de7c6ae6241c933d8c3a6ca86fa7f
opticfluorine <opticfluorine@gmail.com>
2024-05-07 01:03:39 -0400

    Update for release v0.3.0

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -4,28 +4,15 @@ export default function DownloadPage() {
     return <>
         <h1 className={"text-3xl font-bold p-4"}>Downloads</h1>
         <Section className={"max-w-screen-md"}>
-            <h2 className={"text-2xl font-bold"}>Current Release: Sovereign Engine v0.2.0</h2>
+            <h2 className={"text-2xl font-bold"}>Current Release: Sovereign Engine v0.3.0</h2>
             <div className={"p-4"}>
                 <p className={"mb-4"}>This is a development prerelease of Sovereign Engine.
                     This release includes the following new features:</p>
 
                 <ul className={"list-disc"}>
-                    <li>Add login GUI.</li>
-                    <li>Add registration GUI.</li>
-                    <li>Add player selection GUI.</li>
-                    <li>Add player creation GUI.</li>
-                    <li>Add support for deleting players.</li>
-                    <li>Add player debug window.</li>
-                    <li>Add entity debug window.</li>
-                    <li>Make client connection information configurable from configuration file.</li>
-                </ul>
-
-                <p>This release includes the following bugfixes:</p>
-
-                <ul className={"list-disc"}>
-                    <li>Fix multiple issues with world segment load/unload.</li>
-                    <li>Fix issues with missing client data in release binaries.</li>
-                    <li>Fix missing runtime dependencies in release binaries.</li>
+                    <li>Add in-game chat.</li>
+                    <li>Add admin roles for users.</li>
+                    <li>Add integrated resource editor for client-side resources.</li>
                 </ul>
             </div>
             <div>
@@ -33,31 +20,31 @@ export default function DownloadPage() {
                 <ul>
                     <li>Client (Windows 64-bit):
                         <a className={"font-bold"}
-                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.2.0/SovereignClient-v0.2.0-win-x64.zip"}>
-                            SovereignClient-v0.2.0-win-x64.zip</a></li>
+                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.3.0/SovereignClient-v0.3.0-win-x64.zip"}>
+                            SovereignClient-v0.3.0-win-x64.zip</a></li>
                     <li>Server (Windows 64-bit):
                         <a className={"font-bold"}
-                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.2.0/SovereignServer-v0.2.0-win-x64.zip"}>
-                            SovereignServer-v0.2.0-win-x64.zip</a></li>
+                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.3.0/SovereignServer-v0.3.0-win-x64.zip"}>
+                            SovereignServer-v0.3.0-win-x64.zip</a></li>
                     <li>Client (Linux 64-bit):
                         <a className={"font-bold"}
-                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.2.0/SovereignClient-v0.2.0-linux-x64.tar.gz"}>
-                            SovereignClient-v0.2.0-linux-x64.tar.gz
+                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.3.0/SovereignClient-v0.3.0-linux-x64.tar.gz"}>
+                            SovereignClient-v0.3.0-linux-x64.tar.gz
                         </a></li>
                     <li>Server (Linux 64-bit):
                         <a className={"font-bold"}
-                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.2.0/SovereignServer-v0.2.0-linux-x64.tar.gz"}>
-                            SovereignServer-v0.2.0-linux-x64.tar.gz
+                           href={"https://github.com/opticfluorine/sovereign/releases/download/v0.3.0/SovereignServer-v0.3.0-linux-x64.tar.gz"}>
+                            SovereignServer-v0.3.0-linux-x64.tar.gz
                         </a></li>
                     <li>Source Code (.zip):
                         <a className={"font-bold"}
-                           href={"https://github.com/opticfluorine/sovereign/archive/refs/tags/v0.2.0.zip"}>
-                            v0.2.0.zip
+                           href={"https://github.com/opticfluorine/sovereign/archive/refs/tags/v0.3.0.zip"}>
+                            v0.3.0.zip
                         </a></li>
                     <li>Source Code (.tar.gz):
                         <a className={"font-bold"}
-                           href={"https://github.com/opticfluorine/sovereign/archive/refs/tags/v0.2.0.tar.gz"}>
-                            v0.2.0.tar.gz
+                           href={"https://github.com/opticfluorine/sovereign/archive/refs/tags/v0.3.0.tar.gz"}>
+                            v0.3.0.tar.gz
                         </a></li>
                 </ul>
             </div>
